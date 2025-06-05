7f8bfc63e66def54c5c98e4b809fd7829619638c
Eduardo Salinas <eduardoluis93@gmail.com>
2025-06-05 19:45:45 +0100

    Added the option To export and Import Sessions
    I added the functionality to let you export or import the Sessions Model as a CSV file from the django admin panel.

--- tests_clientes/admin.py
+++ tests_clientes/admin.py
@@ -7,8 +7,11 @@ from django.contrib import admin
 # Importo mis modelos de la app de tests para clientes
 from .models import SesionDelTest, RespuestaDelUsuario, PreguntaGuardadaPorElUsuario
 
+# Esto me permite usar la biblioteca de Import Export de django
+from import_export.admin import ImportExportModelAdmin
+
 # Registro mis modelos de la app de tests para clientes
-admin.site.register(SesionDelTest)
+admin.site.register(SesionDelTest, ImportExportModelAdmin)
 admin.site.register(RespuestaDelUsuario)
 admin.site.register(PreguntaGuardadaPorElUsuario)
 
